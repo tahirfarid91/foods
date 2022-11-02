66bc776fa53bf526362a768ef1d28ff45aa07d3d
tahirfarid91 <tahirfarid91@hotmail.com>
2022-11-02 12:18:42 +0500

    employee authentication

--- app/controllers/employees/sessions_controller.rb
+++ app/controllers/employees/sessions_controller.rb
@@ -9,20 +9,10 @@ class Employees::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   # def create
-  #   employee = Employee.find_by(email: params[:email])
-  #   if employee.present?
-  #       session[:employee_id] = employee.id
-  #       redirect_to home_path, notice: "Logged in successfully"
-  #   else
-  #      flash[:alert] = "Invalid email or password"
-  #      redirect_to new_employee_session_path
-  #   end
   # end
 
   # # DELETE /resource/sign_out
   # def destroy
-  #   session[:employee_id] = nil
-  #   redirect_to new_employee_session_path, notice: "Logged out"
   # end
 
   # protected
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if employee_signed_in? 
+      render 'home/index'
+    else
+      redirect_to new_employee_session_path 
+    end 
   end
 end
